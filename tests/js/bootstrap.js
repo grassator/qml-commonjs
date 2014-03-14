01eb2d7bce09c8fc2973dbd4e3934cc71f268bc0
Dmitriy Kubyshkin <dmitriy@kubyshkin.ru>
2014-03-14 23:41:36 +0700

    Forgot to pull back couple of tests in previous commit.

--- tests/js/bootstrap.js
+++ tests/js/bootstrap.js
@@ -2,18 +2,18 @@ console.log('Start Testing. Version: ' + process.version +
             ". Platform: " + process.platform);
 
 // file doesn't exist
-//try {
-//    var main = require.resolve('./non-existent-file.js');
-//} catch(e) {
-//    console.log("Non-existent modules throw Errors as expected.");
-//}
+try {
+    var main = require.resolve('./non-existent-file.js');
+} catch(e) {
+    console.log("Non-existent modules throw Errors as expected.");
+}
 
 // error inside file
-//try {
-//    var main = require('./main.js');
-//} catch(e) {
-//    console.log("Catching require error works");
-//}
+try {
+    var main = require('./main.js');
+} catch(e) {
+    console.log("Catching require error works");
+}
 
 var assert = require('assert');
 
